16bb94542874da4a0931fe620973081e1e9a7b7a
anurraggg <anuragsengupta02@gmail.com>
2025-10-07 17:23:36 +0530

    added new class

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -136,6 +136,7 @@ export default async function decorate(block) {
   if (navSections) {
     navSections.querySelectorAll(':scope .default-content-wrapper > ul > li').forEach((navSection) => {
       if (navSection.querySelector('ul')) navSection.classList.add('nav-drop');
+      navSection.classList.add('aashirwaad');
       navSection.addEventListener('click', () => {
         if (isDesktop.matches) {
           const expanded = navSection.getAttribute('aria-expanded') === 'true';
